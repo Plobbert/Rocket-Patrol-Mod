24557e23501815f65dba9994d5507fa2be77b2e8
Maxwell Mollison <81993009+Plobbert@users.noreply.github.com>
2021-04-21 06:39:59 -0700

    pls work

---
+++ src/prefabs/MustardMan.js
@@ -0,0 +1,35 @@
+class MustardMan extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.movementSpeed = 3;
+        this.isFiring = false;
+        this.texture = texture;
+        console.log(this.texture);
+    }
+
+    update() {
+        if (this.texture = 'rocket') {
+            if(keyA.isDown) {
+                this.x -= this.movementSpeed;
+            }
+            if(keyD.isDown) {
+                this.x += this.movementSpeed;
+            }
+
+            if(Phaser.Input.Keyboard.JustDown(keyW)) {
+                this.isFiring = true;
+            }
+    
+            this.x = Phaser.Math.Clamp(
+                this.x,
+                borderUISize + borderPadding,
+                game.config.width - borderUISize - borderPadding);
+        }
+    }
+
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
+    }
+}
